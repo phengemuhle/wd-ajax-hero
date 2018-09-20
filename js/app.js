07f049ff79e9fd592a2199c52b688bd8e8911dd4
phengemuhle <phengemuhle@gmail.com>
2018-09-20 12:46:31 -0600

    completed js file

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 (function() {
   'use strict';
 
-  const movies = [];
+  var movies = [];
 
   const renderMovies = function() {
     $('#listings').empty();
@@ -10,23 +10,23 @@
       const $col = $('<div>').addClass('col s6');
       const $card = $('<div>').addClass('card hoverable');
       const $content = $('<div>').addClass('card-content center');
-      const $title = $('<h6>').addClass('card-title truncate');
+      const $Title = $('<h6>').addClass('card-Title truncate');
 
-      $title.attr({
+      $Title.attr({
         'data-position': 'top',
-        'data-tooltip': movie.title
+        'data-tooltip': movie.Title
       });
 
-      $title.tooltip({ delay: 50 }).text(movie.title);
+      $Title.tooltip({ delay: 50 }).text(movie.Title);
 
-      const $poster = $('<img>').addClass('poster');
+      const $Poster = $('<img>').addClass('Poster');
 
-      $poster.attr({
-        src: movie.poster,
-        alt: `${movie.poster} Poster`
+      $Poster.attr({
+        src: movie.Poster,
+        alt: `${movie.Poster} Poster`
       });
 
-      $content.append($title, $poster);
+      $content.append($Title, $Poster);
       $card.append($content);
 
       const $action = $('<div>').addClass('card-action center');
@@ -41,9 +41,9 @@
 
       const $modal = $('<div>').addClass('modal').attr('id', movie.id);
       const $modalContent = $('<div>').addClass('modal-content');
-      const $modalHeader = $('<h4>').text(movie.title);
-      const $movieYear = $('<h6>').text(`Released in ${movie.year}`);
-      const $modalText = $('<p>').text(movie.plot);
+      const $modalHeader = $('<h4>').text(movie.Title);
+      const $movieYear = $('<h6>').text(`Released in ${movie.Year}`);
+      const $modalText = $('<p>').text(movie.Plot);
 
       $modalContent.append($modalHeader, $movieYear, $modalText);
       $modal.append($modalContent);
@@ -55,6 +55,26 @@
       $('.modal-trigger').leanModal();
     }
   };
-
-  // ADD YOUR CODE HERE
+  const proxy = "https://cors-anywhere.herokuapp.com/"
+  var submit = document.querySelector('#submit');
+  var form = document.querySelector('form');
+  var input = document.querySelector('#search');
+  var https = new XMLHttpRequest();
+  
+  submit.addEventListener('click', submission);
+  function submission(event){
+    event.preventDefault();
+    var film = input.value;
+    fetch('https://omdb-api.now.sh/?s=' + encodeURI(film))
+      .then((response) => response.json())
+      .then((function(result){
+        for(let i = 0; i < result.Search.length; i++ ){
+        movies.unshift(result.Search[i])}
+        renderMovies()
+      }))
+      .catch((error) => console.log(error))
+      console.log(movies)
+      movies = []
+  } 
 })();
+
